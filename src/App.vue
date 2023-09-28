af130903dc25aa5125a14f04a5cb28cf2467339a
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-28 14:43:36 +0200

    milestone 1
    stampati in pagina titolo orig, titolo, lingua, voto

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 import { state } from './state.js'
 
 export default {
+  name: 'App',
   data() {
     return {
-      state
+      state,
     }
   },
 }
@@ -21,7 +22,7 @@ export default {
 
     <div class="container py-4">
 
-      <div class="row">
+      <div class="row mb-3">
         <div class="col-6">
           <div class="input-group">
             <input type="text" class="form-control" placeholder="Digita lettere o parole contenute nel titolo"
@@ -33,8 +34,23 @@ export default {
         </div>
       </div>
 
-    </div>
+      <div class="row g-2">
+        <div class="col-3" v-for="movie in state.movieArray">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">{{ movie.original_title }} ({{ movie.title }})</h5>
+              <p class="card-text">
+              <ul>
+                <li>Lingua: {{ movie.original_language }}</li>
+                <li>Voto: {{ Math.round(Number(movie.vote_average) * 10) / 10 }}</li>
+              </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
+    </div>
 
   </main>
 </template>
--- src/state.js
+++ src/state.js
@@ -2,16 +2,19 @@ import { reactive } from 'vue'
 import axios from 'axios'
 
 export const state = reactive({
-    base_url: 'https://api.themoviedb.org/3/search/movie?api_key=2ac860428212e4c9593f814d1c5e9efe',
+    base_url: 'https://api.themoviedb.org/3/search/movie?api_key=2ac860428212e4c9593f814d1c5e9efe&language=it-IT',
     url: '',
-    movieQuery: '',
+    movieQuery: 'matrix',
+    movieArray: [],
 
     fetchMovieData() {
         this.url = [this.base_url + '&query=' + this.movieQuery];
         axios.
             get(this.url)
             .then(response => {
-                console.log(response);
+                this.movieArray = response.data.results;
+
+                console.log(this.movieArray);
             })
             .catch(error => {
                 console.log(error);
