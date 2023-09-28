38fd882ed3cd9ddbcc0ee44b358382c4e045c98a
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-28 17:34:05 +0200

    milestone 2 pt 2
    aggiunta ricerca serie TV

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,23 @@ export default {
     return {
       state,
       languageToCountry,
-      countriesFlags
+      countriesFlags,
+      messageInfo: 'Digitare parole chiave e premere il tasto "Cerca"',
+      messageError: ''
     }
   },
   methods: {
-
-    funzione(lang) {
+    sendQuery() {
+      this.messageInfo = '';
+      if (state.query == '') {
+        this.messageError = 'Digitare delle parole chiave nella barra di ricerca';
+      } else {
+        this.messageError = '';
+      }
+      state.fetchMovieData()
+      state.fetchTVSeriesData()
+    },
+    searchFlag(lang) {
       let index = languageToCountry.language.indexOf(lang, 0);
       if (index == -1) {
         return false
@@ -45,31 +56,69 @@ export default {
         <div class="col-6">
           <div class="input-group">
             <input type="text" class="form-control" placeholder="Digita lettere o parole contenute nel titolo"
-              v-model="state.movieQuery">
-            <button class="btn btn-primary" type="button" id="btnMovie" @click="state.fetchMovieData()">
-              Cerca film
+              v-model="state.query">
+            <button class="btn btn-primary" type="button" id="btnMovie" @click="this.sendQuery()">
+              Cerca
             </button>
           </div>
         </div>
       </div>
 
-      <div class="row g-2">
-        <div class="col-12 text-center text-danger" v-if="state.movieQuery == '' && state.movieArray.length == 0">
-          <h2>Digitare delle parole chiave nella barra di ricerca</h2>
-        </div>
-        <div class="col-3" v-for="movie in state.movieArray" v-else>
+      <div class="w-100 text-center text-info mb-3">
+        <h2>{{ messageInfo }}</h2>
+      </div>
+
+      <div class="w-100 text-center text-danger mb-3">
+        <h2>{{ messageError }}</h2>
+      </div>
+
+      <div class="mb-3">
+        <h2>Film</h2>
+      </div>
+      <div class="mb-3" v-if="messageInfo == ''">
+        <h4>La ricerca ha prodotto {{ state.movieArray.length }} risultati</h4>
+      </div>
+      <div class="row mb-3">
+        <div class="col-3" v-for="movie in state.movieArray" v-if="state.movieArray.length > 0">
           <div class="card h-100">
             <div class="card-body">
-              <h5 class="card-title">{{ movie.original_title }} ({{ movie.title }})</h5>
+              <h5 class="card-title">{{ movie.title }} ({{ movie.original_title }})</h5>
               <p class="card-text">
-              <ul>
-                <li v-if="this.funzione(movie.original_language)">
-                  Lingua: <img :src="this.funzione(movie.original_language)" alt="">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item" v-if="this.searchFlag(movie.original_language)">
+                  Lingua: <img :src="this.searchFlag(movie.original_language)" height="20" alt="">
                 </li>
-                <li v-else>
+                <li class="list-group-item" v-else>
                   Lingua: {{ movie.original_language }}
                 </li>
-                <li>Voto: {{ Math.round(Number(movie.vote_average) * 10) / 10 }}</li>
+                <li class="list-group-item">Voto: {{ Math.round(Number(movie.vote_average) * 10) / 10 }}</li>
+              </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <h2>Serie TV</h2>
+      </div>
+      <div class="mb-3" v-if="messageInfo == ''">
+        <h4>La ricerca ha prodotto {{ state.TVSeriesArray.length }} risultati</h4>
+      </div>
+      <div class="row mb-3">
+        <div class="col-3" v-for="series in state.TVSeriesArray" v-if="state.TVSeriesArray.length > 0">
+          <div class="card h-100">
+            <div class="card-body">
+              <h5 class="card-title">{{ series.name }} ({{ series.original_name }})</h5>
+              <p class="card-text">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item" v-if="this.searchFlag(series.original_language)">
+                  Lingua: <img :src="this.searchFlag(series.original_language)" height="20" alt="">
+                </li>
+                <li class="list-group-item" v-else>
+                  Lingua: {{ series.original_language }}
+                </li>
+                <li class="list-group-item">Voto: {{ Math.round(Number(series.vote_average) * 10) / 10 }}</li>
               </ul>
               </p>
             </div>
--- src/state.js
+++ src/state.js
@@ -2,24 +2,38 @@ import { reactive } from 'vue'
 import axios from 'axios'
 
 export const state = reactive({
-    base_url: 'https://api.themoviedb.org/3/search/movie?api_key=2ac860428212e4c9593f814d1c5e9efe&language=it-IT',
-    url: '',
-    movieQuery: 'matrix',
+    query: 'matrix',
+
+    baseMovieUrl: 'https://api.themoviedb.org/3/search/movie?api_key=2ac860428212e4c9593f814d1c5e9efe&language=it-IT',
+    movieUrl: '',
     movieArray: [],
 
+    baseTVSeriesUrl: 'https://api.themoviedb.org/3/search/tv?api_key=2ac860428212e4c9593f814d1c5e9efe&language=it_IT',
+    TVSeriesUrl: '',
+    TVSeriesArray: [],
+
     fetchMovieData() {
-        this.url = [this.base_url + '&query=' + this.movieQuery];
-        console.log(this.movieArray);
+        this.movieUrl = [this.baseMovieUrl + '&query=' + this.query];
         axios.
-            get(this.url)
+            get(this.movieUrl)
             .then(response => {
                 this.movieArray = response.data.results;
-
                 console.log(this.movieArray);
             })
             .catch(error => {
                 console.log(error);
             })
+    },
+    fetchTVSeriesData() {
+        this.TVSeriesUrl = [this.baseTVSeriesUrl + '&query=' + this.query];
+        axios.
+            get(this.TVSeriesUrl)
+            .then(response => {
+                this.TVSeriesArray = response.data.results;
+                console.log(state.TVSeriesArray);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
-
 })
