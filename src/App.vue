66c23be60b8861355d6d322163ec8ac6c65afb9d
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-28 12:10:17 +0200

    initial commit
    inizializzata app con sass e bootstrap

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<template>
+  <header class="p-4">
+
+    <h1>Boolflix</h1>
+
+  </header>
+
+  <main>
+
+    <div class="container py-4">
+      ciao
+    </div>
+
+  </main>
+</template>
+
+<style lang="sass"></style>
