557e804ecaa2b1b37ad0f2b45d1c4235da2062e4
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-28 12:42:37 +0200

    axios
    installato axios e generata chiamata GET all'API di movie DB

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@ export default {
     return {
       state
     }
+  },
+  created() {
+    state.fetchMovie();
   }
 }
 </script>
--- src/state.js
+++ src/state.js
@@ -1,5 +1,19 @@
 import { reactive } from 'vue'
+import axios from 'axios'
 
 export const state = reactive({
-    message: 'state importato corrett'
+    message: 'state importato corrett',
+    base_url: 'https://api.themoviedb.org/3/search/movie?api_key=2ac860428212e4c9593f814d1c5e9efe&query=matrix+reloaded',
+
+    fetchMovie() {
+        axios.
+            get(this.base_url)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
 })
